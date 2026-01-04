f2a30e476653b7901dafbecd5396376933f3b920
Prathamesh Mhatugade <prathameshgit2@gmail.com>
2026-01-04 15:44:01 +0530

    Added Styling

--- src/Components/AddImageForm.js
+++ src/Components/AddImageForm.js
@@ -1,5 +1,13 @@
 function AddImageForm() {
-  return <div>AddImageForm</div>;
+  return (
+    <div className="add-image-form">
+      <form>
+        <input type="text" placeholder="Title" />
+        <input type="url" placeholder="Image-Url" />
+        <button type="submit">Add-Image</button>
+      </form>
+    </div>
+  );
 }
 
 export default AddImageForm;
